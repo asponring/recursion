d8eb82366a33e867711d754e75919116c2d03823
Andy Sponring <asponring@gmail.com>
2015-03-06 11:17:40 -0800

    Completed stringifyJSON

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -5,4 +5,45 @@
 
 var stringifyJSON = function(obj) {
   // your code goes here
+  var JSONstr = "";
+  
+  if (Array.isArray(obj)) {
+    //do something
+    JSONstr += "[";
+
+    for (var i = 0; i < obj.length; i++) {
+      JSONstr += stringifyJSON(obj[i]) + ",";
+    }
+    if (i > 0) {
+      JSONstr = JSONstr.slice(0, JSONstr.length - 1);
+    }
+    JSONstr += "]";
+  } else if (typeof obj === "object" && obj !== null) {
+    //do other stuff
+    JSONstr += "{";
+
+    for (var prop in obj) {
+      var temp = stringifyJSON(obj[prop]);
+      if (temp === undefined) {
+        continue;
+      }
+      JSONstr += "\"" + prop + "\"" + ":";
+      JSONstr += stringifyJSON(obj[prop]) + ",";
+    }
+    if (JSONstr.charAt(JSONstr.length - 1) === ",") {
+      JSONstr = JSONstr.slice(0, JSONstr.length - 1);
+    }
+    JSONstr += "}";
+  } else if (typeof obj === "string") {
+    //do stuff
+    JSONstr += "\"" + obj + "\"";
+  } else if (typeof obj === "function" || obj === undefined) {
+    //indicate that this isn't right
+    return;
+  } else {
+    //for number, boolean or null values
+    JSONstr += obj;
+  }
+  
+  return JSONstr;
 };
